5442de74cddb9cadb50ef84e167a48d1db5a95d3
Liarr <Liarr@foxmail.com>
2022-10-04 00:41:40 +0800

    主页

--- app/src/main/java/com/example/travelprevention/MainActivity.kt
+++ app/src/main/java/com/example/travelprevention/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.travelprevention
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alibaba.android.arouter.launcher.ARouter
+import com.example.appbase.config.ArouterConfig
+import com.example.appbase.ui.BaseActivity
+import com.example.travelprevention.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initClick()
+    }
+
+    private fun initClick() {
+        // 查询核酸检测机构
+        mViewBinding.llAgency.setOnClickListener {
+            ARouter.getInstance().build(ArouterConfig.AGENCY_MESSAGE).navigation()
+        }
+        // 查询风险等级地区
+        mViewBinding.llRiskLevel.setOnClickListener {
+            ARouter.getInstance().build(ArouterConfig.RISK_LEVEL).navigation()
+        }
+        // 查询出行政策
+        mViewBinding.llTravel.setOnClickListener {
+            ARouter.getInstance().build(ArouterConfig.TRAVEL_POLICY).navigation()
+        }
+    }
+
+    override fun getViewBinding(): ActivityMainBinding {
+        return ActivityMainBinding.inflate(layoutInflater)
     }
 }
